168dc844401d3587d3dc8d69622f0c7493f37dd9
vovmr <37941846+vovmr@users.noreply.github.com>
2020-01-01 20:56:12 +0300

    Create Template_ordered_set.cpp

---
+++ Template_ordered_set.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define ll long long
+#define ld long double
+#define all(x)              (x).begin(), (x).end()
+#define inf                 1e18
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree <T, null_type, less <T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//    ifstream cin("input.txt");
+//    ofstream cout("output.txt");
+}
