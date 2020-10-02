704bb517f32a69149c88544bf402c2bfd4a50979
vovmr <37941846+vovmr@users.noreply.github.com>
2020-10-03 02:56:33 +0300

    Create Modular inverse from 1 to n..cpp

---
+++ Modular inverse from 1 to n..cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define ll long long
+#define ld long double
+#define all(x)              (x).begin(), (x).end()
+#define inf                 1e18
+
+using namespace std;
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+void solve() {
+	const ll p = 998244353;
+	int n;
+	cin >> n;
+	vector <ll> iv(n + 1); iv[0] = iv[1] = 1;
+	for (int i = 2; i <= n; ++i) {
+		iv[i] = (p - p / i) * iv[p % i] % p;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//	ifstream cin("input.txt");
+//	ofstream cout("output.txt");
+	int q = 1; // cin >> q;
+	while (q--) solve();
+}
