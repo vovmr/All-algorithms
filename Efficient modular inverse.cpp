212fa2c0a21742e559829d5eee85aa1afc9cd7f2
vovmr <37941846+vovmr@users.noreply.github.com>
2020-10-03 00:58:17 +0300

    Create Efficient modular inverse.cpp

---
+++ Efficient modular inverse.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define ll long long
+#define ld long double
+#define all(x)              (x).begin(), (x).end()
+#define inf                 1e18
+
+using namespace std;
+
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+const ll mod = 1e9 + 7;
+
+inline ll bp(ll a, ll n) {
+	ll res = 1;
+	while (n) {
+		if (n & 1) res *= a, res %= mod;
+		a *= a, a %= mod;
+		n >>= 1;
+	}
+	return res;
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	vector <ll> iv(n); iv[0] = iv[1] = 1;
+	for (ll i = 2; i < n; ++i) {
+		iv[i] = (mod - (mod / i)) * iv[mod % i] % mod;
+		/// faster then bp(i, mod - 2)
+		assert((i * iv[i]) % mod == 1);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//	ifstream cin("input.txt");
+//	ofstream cout("output.txt");
+	int q = 1; // cin >> q;
+	while (q--) solve();
+}
